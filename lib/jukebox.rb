b30fa5d391bedc10bea119839ff2eeb2a789973b
zara ahmadi <zatokey@gmail.com>
2020-05-22 04:31:06 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,11 +12,13 @@ songs = [
 ]
 
 def help
+  
  puts "I accept the following commands:"
 puts "- help : displays this help message"
 puts "- list : displays a list of songs you can play"
 puts"- play : lets you choose a song to play"
 puts"- exit : exits this program"
+
 end
  
  def help(songs)
