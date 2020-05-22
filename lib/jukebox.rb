2e9cbe97a2b5f89d1d4e68e23a65c3100c47302d
zara ahmadi <zatokey@gmail.com>
2020-05-22 04:06:43 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,11 +12,15 @@ songs = [
 ]
 
 def help
-  "Hi #{name}!"
+ puts "I accept the following commands:"
+puts "- help : displays this help message"
+puts "- list : displays a list of songs you can play"
+puts"- play : lets you choose a song to play"
+puts"- exit : exits this program"
 end
  
- def help
-  "Hi #{name}!"
+ def help(song)
+  song.each_with
 end
  
  def help
